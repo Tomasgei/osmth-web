f061fa6614cfad7fc89aa9fd3a2f28228732439e
Tomasgei <50343160+Tomasgei@users.noreply.github.com>
2023-04-03 01:30:38 +0200

    static

--- core/urls.py
+++ core/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('web/', include("web.urls", namespace="web")),
     path('admin/', admin.site.urls),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
